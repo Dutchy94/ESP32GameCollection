976d7fc221ebe95be2104e96a89bf1c32e09bf6a
Dutchy94 <127901125+Dutchy94@users.noreply.github.com>
2025-05-01 00:11:29 +0200

    Update protocol.h

--- common/protocol.h
+++ common/protocol.h
@@ -2,9 +2,12 @@
 
 #include <Arduino.h>
 
-#define MESSAGE_TYPE_COMMAND 0x01
+#define MESSAGE_TYPE_COMMAND     0x01
+#define CLAIM_REQUEST            0x10
+#define CLAIM_RESPONSE           0x11
 
 struct GameMessage {
-    uint8_t type;
-    uint8_t command;
+  uint8_t type;
+  uint8_t command;
+  uint8_t value;  // z. B. Modul-ID
 };
